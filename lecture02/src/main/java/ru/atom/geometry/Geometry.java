28943090631b9fb9d4aa6b0f81443a0eeed9f437
AndreyCh <propre@mail.ru>
2018-11-19 22:55:41 +0300

    temp

--- lecture02/src/main/java/ru/atom/geometry/Geometry.java
+++ lecture02/src/main/java/ru/atom/geometry/Geometry.java
@@ -9,9 +9,31 @@ package ru.atom.geometry;
  *  .---------->
  */
 
-public final class Geometry {
-    
-    private Geometry() {
+public final class Geometry implements Collider{
+//    private Point p1=null, p2=null;
+    private int x1=0,y1=0,x2=0,y2=0;
+
+    public Geometry(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public int getX1() {
+        return x1;
+    }
+
+    public int getY1() {
+        return y1;
+    }
+
+    public int getX2() {
+        return x2;
+    }
+
+    public int getY2() {
+        return y2;
     }
 
     /**
@@ -22,7 +44,8 @@ public final class Geometry {
      * @return new Bar
      */
     public static Collider createBar(int firstCornerX, int firstCornerY, int secondCornerX, int secondCornerY) {
-        throw new UnsupportedOperationException();
+        return new Geometry(firstCornerX,firstCornerY,secondCornerX,secondCornerY);
+//        throw new UnsupportedOperationException();
     }
 
     /**
@@ -30,6 +53,40 @@ public final class Geometry {
      * @return new Point
      */
     public static Collider createPoint(int x, int y) {
-        throw new UnsupportedOperationException();
+        return  new Point(x, y);
+//        throw new UnsupportedOperationException();
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        if(obj instanceof Geometry) return
+                (this.x1==((Geometry) obj).getX1() & this.y1==((Geometry) obj).getY1()&
+                 this.x2==((Geometry) obj).getX2() & this.y2==((Geometry) obj).getY2());
+        return false;
+    }
+
+    @Override
+    public boolean isColliding(Collider other) {
+        if (this == other) return true;
+        if (other == null ) return false;
+        if(other instanceof Point)
+            return ((((Point) other).getX() >= this.getX1() & ((Point) other).getX() <= this.getX2()) &
+                    (((Point) other).getY() >= this.getY1() & ((Point) other).getY() <= this.getY2()));
+        if(other instanceof Geometry)
+            return ((((Geometry) other).getX1() >= this.getX1() & ((Geometry) other).getX1() <= this.getX2()) &
+                    (((Geometry) other).getY1() >= this.getY1() & ((Geometry) other).getY1() <= this.getY2())|
+                    (((Geometry) other).getX2() >= this.getX1() & ((Geometry) other).getX2() <= this.getX2()) &
+                    (((Geometry) other).getY2() >= this.getY1() & ((Geometry) other).getY2() <= this.getY2())
+
+//                    (((Geometry) other).getX1() <= this.getX1() & ((Geometry) other).getX1() >= this.getX2()) &
+//                    (((Geometry) other).getY1() <= this.getY1() & ((Geometry) other).getY1() >= this.getY2())|
+//                    (((Geometry) other).getX2() <= this.getX1() & ((Geometry) other).getX2() >= this.getX2()) &
+//                    (((Geometry) other).getY2() <= this.getY1() & ((Geometry) other).getY2() >= this.getY2())
+            );
+        return false;
+    }
+
 }
--- lecture02/src/main/java/ru/atom/geometry/Point.java
+++ lecture02/src/main/java/ru/atom/geometry/Point.java
@@ -3,9 +3,24 @@ package ru.atom.geometry;
 /**
  * Template class for
  */
-public class Point /* super class and interfaces here if necessary */ {
+public class Point implements Collider/* super class and interfaces here if necessary */ {
+    int x=0, y=0;
     // fields
     // and methods
+
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 
     /**
      * @param o - other object to check equality with
@@ -16,10 +31,31 @@ public class Point /* super class and interfaces here if necessary */ {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
+        if(o instanceof Point) return (this.x==((Point) o).getX() & this.y==((Point) o).getY());
+
         // cast from Object to Point
-        Point point = (Point) o;
+//        Point point = (Point) o;
 
         // your code here
         throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isColliding(Collider other) {
+        if (this == other) return true;
+        if (other == null) return false;
+
+        if(other instanceof Point) return (this.x==((Point) other).getX() & this.y==((Point) other).getY());
+        if(other instanceof Geometry) {
+            return (this.x >= ((Geometry) other).getX1() & this.x <= ((Geometry) other).getX2()) &
+                    (this.y >= ((Geometry) other).getY1() & this.y <= ((Geometry) other).getY2());
+        }
+        return false;
+
+        // cast from Object to Point
+//        Point point = (Point) o;
+
+        // your code here
+//        throw new UnsupportedOperationException();
     }
 }
